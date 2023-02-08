a5ccc9771064221f92135c8fc314dce632e192a2
Daniel Sudzilouski <daniel@sudzilouski.com>
2023-02-07 19:21:18 -0500

    implement branch and bound with backtracking

--- assignments/assignment-2/bab/bab_starter.py
+++ assignments/assignment-2/bab/bab_starter.py
@@ -1,13 +1,6 @@
-import picos as pic
-from picos import RealVariable
-from copy import deepcopy
-from heapq import *
-import heapq as hq
-import numpy as np
-import itertools
+from picos import RealVariable, SolutionFailure, Problem
 import math
 
-counter = itertools.count()
 
 class BBTreeNode():
     """
@@ -15,7 +8,7 @@ class BBTreeNode():
     to determine the optimal result and corresponding best variable
     values.
 
-    Attributes:
+    Constructor:
         vars (list of picos RealVariable objects): variables in the
             problem.
         constraints (list of constraints): list of problem constraints.
@@ -31,99 +24,53 @@ class BBTreeNode():
         Initializes BBTreeNode.
         """
         self.vars = vars
-        self.constraints = constraints
-        self.objective = objective
-        self.prob = prob
+        self.prob=Problem()
+        self.prob.add_list_of_constraints(constraints)
+        self.prob.set_objective('max', objective)
+        self.best_res = -1e20
+        self.best_vars = self.__copy_vars(vars)
 
-    def __deepcopy__(self):
-        """
-        Deepcopies the picos problem.
-        This overrides the system's deepcopy method bc it doesn't work
-        on classes by itself.
-
-        Returns:
-            (BBTreeNode object): copy of BBTreeNode.
-        """
-        newprob = pic.Problem.clone(self.prob)
-        return BBTreeNode(self.vars, newprob.constraints, self.objective, newprob)
-
-    def buildProblem(self):
-        """
-        Builds the initial Picos problem.
-
-        Returns:
-            self.prob (picos Problem object): problem created from
-                constraints, objective, and vars.
-        """
-        prob=pic.Problem()
-        prob.add_list_of_constraints(self.constraints)
-        prob.set_objective('max', self.objective)
-        self.prob = prob
-        return self.prob
-
-    def is_integral(self):
-        """
-        Checks if all variables (excluding the one we're maxing) are
-        integers.
-
-        Returns:
-            (bool): returns True if all variables (excluding the one
-                we're maximizing) are integers, otherwise False.
-        """
-        for v in self.vars[:-1]:
-            if v.value == None or abs(round(v.value) - float(v.value)) > 1e-4:
-                return False
-        return True
-
-    def branch_floor(self, branch_var):
-        """
-        Makes a child where xi <= floor(xi).
-
-        Args:
-            branch_var (float): variable to branch on.
-
-        Returns:
-            n1 (BBTreeNode object): child where xi <= floor(xi).
-        """
-        n1 = deepcopy(self)
-        # add in the new binary constraint
-        n1.prob.add_constraint(branch_var <= math.floor(branch_var.value))
-        return n1
-
-    def branch_ceil(self, branch_var):
-        """
-        Makes a child where xi >= ceiling(xi).
-
-        Args:
-            branch_var (float): variable to branch on.
-
-        Returns:
-            n2 (BBTreeNode object): child where xi >= ceiling(xi).
-        """
-        n2 = deepcopy(self)
-        # add in the new binary constraint
-        n2.prob.add_constraint(branch_var >= math.ceil(branch_var.value))
-        return n2
+    """
+        Casts a list of picos RealVariables objects to a python list of floats.
+    """
+    @staticmethod
+    def __copy_vars(vars):
+        return list(map(lambda x: x.value, vars))
 
     def bbsolve(self):
         """
         Uses the branch and bound method to solve an integer program.
 
         Returns:
-            bestres (float): value of the maximized objective function.
+            best_res (float): value of the maximized objective function.
             bestnode_vars (list of floats): list of variables that
-                create bestres.
+                create best_res.
         """
-        # these lines build up the initial problem and add it to a heap
-        root = self
-        res = root.buildProblem().solve(solver='cvxopt')
-        heap = [(res, next(counter), root)]
-        # set bestres to an arbitrary small initial best objective value
-        bestres = -1e20
-        # initialize bestnode_vars to the root vars
-        bestnode_vars = root.vars
+        try:
+            # solves the current node
+            solve = self.prob.solve(solver='cvxopt') 
+            # determines if at least one real variable exists
+            real_variable = next((v for v in self.vars if abs(round(v.value) - float(v.value)) > 1e-4), None)
+            # update best solution
+            if(not real_variable and solve.reportedValue > self.best_res + 1e-4):
+                self.best_res = solve.reportedValue
+                self.best_vars = self.__copy_vars(self.vars)
+            # branch and bound recursively
+            elif(solve.reportedValue > self.best_res + 1e-4):
+                # add and backtrack less than discrete constraint
+                less_than = (real_variable <= math.floor(real_variable.value))
+                self.prob += less_than
+                self.bbsolve()
+                self.prob.remove_constraint(less_than)
 
-        #TODO:
-        # Implement your solution here!
+                # add and backtrack greater than discrete constraint
+                greater_than = (real_variable >= math.ceil(real_variable.value))
+                self.prob += greater_than
+                self.bbsolve()
+                self.prob.remove_constraint(greater_than)
 
-        return bestres, bestnode_vars
+        # solution is infeasible, discard node
+        except SolutionFailure:
+                pass
+
+        return self.best_res, self.best_vars
